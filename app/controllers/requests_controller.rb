66f770acc8f30e9ffa735be474675a72de3d06ea
David Hasson <dhassonlocal@alys.local>
2009-04-06 14:07:24 -0700

    Changed time fields to use unbound form helpers, set in controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -16,10 +16,22 @@ class RequestsController < ApplicationController
   end 
 
   def create
-    time_methods = [:assembly_time1, :assembly_time2, :assembly_time3]
+    time_methods = [:assembly_time1, :assembly_time2, :assembly_time3, :lunch_end, :lunch_start]
     tries = 0
     begin
-      logger.error(params.inspect + "\n\n")
+      #logger.error(params.inspect + "\n\n")
+      time_methods.each do |t|
+        # If they did the hour, but forgot the minute, set it to zero
+        if params[t] && ! params[t][:hour].blank? && params[t][:minute].blank?
+          params[t][:minute] = "0"
+        end
+        # Set the real param in the request section only if they specified a time
+        if params[t] && ! params[t][:hour].blank? && ! params[t][:minute].blank?
+          params[:request][t] = "#{params[t][:hour]}:#{params[t][:minute]}:00"
+        else
+          params[:request][t] = nil
+        end
+      end
       @request = Request.new(params[:request])
     # Trap multi_errors from dates and log them prettierly
     rescue => multi_errors
@@ -28,35 +40,35 @@ class RequestsController < ApplicationController
         logger.error("***\n#{assign_error.exception.class} (#{assign_error.exception.message}):\n   " +
           clean_backtrace(assign_error.exception).join("\n    ") + "\n" +
           "Attribute: #{assign_error.attribute}\n")
-        a = assign_error.attribute
-        if time_methods.include?(a.to_sym)
-          logger.error("Deleting #{a} from params and retrying")
-          # If we see a time specified, add current date and retry, else nil it out
-          if(!  (params[:request][ a.to_s + "(4i)" ].empty? || params[:request][ a.to_s + "(5i)" ].empty? ))
-            b = Date.today            
-            params[:request][ a.to_s + "(1i)" ] = b.year
-            params[:request][ a.to_s + "(2i)" ] = b.month
-            params[:request][ a.to_s + "(3i)" ] = b.day
-          else
-            params[:request].delete( a.to_s + "(1i)" )
-            params[:request].delete( a.to_s + "(2i)" )
-            params[:request].delete( a.to_s + "(3i)" )
-            params[:request].delete( a.to_s + "(4i)" )
-            params[:request].delete( a.to_s + "(5i)" )
-          end
-        end
-        
+#        a = assign_error.attribute
+#        if time_methods.include?(a.to_sym)
+#          # If we see a time specified, add current date and retry, else nil it out
+#          if(!  (params[:request][ a.to_s + "(4i)" ].nil? || params[:request][ a.to_s + "(5i)" ].nil? ) )
+#            logger.error("Setting date on #{a} and retrying")
+#            b = Date.today            
+#            params[:request][ a.to_s + "(1i)" ] = b.year
+#            params[:request][ a.to_s + "(2i)" ] = b.month
+#            params[:request][ a.to_s + "(3i)" ] = b.day
+#          else
+#            logger.error("Deleting #{a} from params and retrying")
+#            params[:request].delete( a.to_s + "(1i)" )
+#            params[:request].delete( a.to_s + "(2i)" )
+#            params[:request].delete( a.to_s + "(3i)" )
+#            params[:request].delete( a.to_s + "(4i)" )
+#            params[:request].delete( a.to_s + "(5i)" )
+#          end
+#        end      
       end
       logger.fatal("#{multi_errors.class} (#{multi_errors.message}):\n" + 
         clean_backtrace(multi_errors).join("\n    ") + "\n")
       
       # Retry once in case the code cleaned it up, else raise
-      tries += 1
-      unless tries < 2
-        logger.error("* Won't retry anymore *\n")
+#      tries += 1
+#      unless tries < 2
+#        logger.error("* Won't retry anymore *\n")
         raise
-      end
-      retry
+#      end
+#      retry
     else
       if @request.save
         flash[:notice] = 'Request was successfully created.'
