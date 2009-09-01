f50c3a1d3b2eb4963791846bd0f83d4b41c9b560
David Hasson <funtaff@colbie.virtmachine.com>
2009-09-01 12:09:09 -0700

    Added mail relay and change from address

--- app/models/mailman.rb
+++ app/models/mailman.rb
@@ -2,14 +2,14 @@ class Mailman < ActionMailer::Base
   def request_notification(request)
     recipients ["David Hasson <dph@funtaff.net>", "Beetlebug Books <beetlebugbooks@cox.net>", "David Hasson <david.hasson@loni.ucla.edu>", "Author Mike <authormrmike@gmail.com>", "Amanda Hammond <arhammond@gmail.com>"]
     subject "[beetlebug] New Request Submission"
-    from "Beetlebug Requests <mailer@beetlebug.net>"
+    from "Beetlebug Requests <mailer@beetlebugbooks.com>"
     body :request => request
   end
   
   def request_received(request)
     recipients "#{request.contact1_first} #{request.contact1_last} <#{request.contact1_email}>"
     subject "[beetlebug] Thanks for your assembly request!"
-    from "Beetlebug Requests <mailer@beetlebug.net>"
+    from "Beetlebug Requests <mailer@beetlebugbooks.com>"
     body :request => request
   end
 end
