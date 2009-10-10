9c35ff803b9462a9cf6a06aa3bb2fb536a079512
David Hasson <dhassonlocal@alys.local>
2009-10-10 13:52:38 -0700

    Added infrastructure for jQuery tabs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,31 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Application-wide functions
+
+// Initialize jQuery UI Elements
+$(document).ready( function() {
+    $("#tabs").tabs();
+});
+
+function next_tab() {
+    var $tabs = $('#tabs').tabs();
+
+    // In case we're already on the last tab
+    if($tabs.tabs('option', 'selected') == ( $tabs.tabs('length') - 1) ) { 
+        return false;
+    }   
+
+    $tabs.tabs('select', $tabs.tabs('option', 'selected') + 1); 
+    return false;
+}
+
+function prev_tab() {
+    var $tabs = $('#tabs').tabs();
+
+    // In case we're already on the first tab
+    if($tabs.tabs('option', 'selected') == 0) {
+        return false;
+    }   
+
+    $tabs.tabs('select', $tabs.tabs('option', 'selected') - 1); 
+    return false;
+}
+
